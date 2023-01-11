97e35ac5f4af18808f68aa5502735aa2e4497c5a
Adam Marx <adamalexmarx@gmail.com>
2023-01-10 20:51:38 -0800

    Added better spacing

--- eqArrays.js
+++ eqArrays.js
@@ -9,15 +9,15 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
 
-if(array1.length !== array2.length) return false;
+  if(array1.length !== array2.length) return false;
 
-for(let index = 0; index < array1.length; index++) {
-      if(array1[index] !== array2[index]){
-        return false;
-      } 
+  for(let index = 0; index < array1.length; index++) {
+        if(array1[index] !== array2[index]){
+          return false;
+        } 
+    }
+    return true;
   }
-  return true;
-}
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 
