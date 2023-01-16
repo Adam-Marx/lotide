3764224dff62ac3617e2c4d1f25176716717e016
Adam Marx <adamalexmarx@gmail.com>
2023-01-16 15:06:26 -0800

    Refactored with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,12 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-
-const testArray = [1, 2, 3, 4];
-assertArraysEqual(middle(testArray), 2.5);
-const testArray2 = [1, 2, 3];
-assertArraysEqual(middle(testArray2), 2);
+const assert = require('chai').assert
+describe('#middle', () => {
+  it('returns 2.5 for [1, 2, 3, 4]', () => {
+    const testArray = [1, 2, 3, 4];
+    assert.deepEqual(middle(testArray), 2.5);
+  });
+  it('returns 2 for [1, 2, 3]', () => {
+    const testArray = [1, 2, 3];
+    assert.deepEqual(middle(testArray), 2);
+  });
+});
