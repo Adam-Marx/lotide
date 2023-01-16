c0049a22191aef1372c4509859f3c5421ee6b3ed
Adam Marx <adamalexmarx@gmail.com>
2023-01-16 13:44:21 -0800

    Refactored using require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-
- const eqArrays = function(array1, array2) {
-
-  if(array1.length !== array2.length) return false;
-
-  for(let index = 0; index < array1.length; index++) {
-    if(array1[index] !== array2[index]){
-      return false;
-    } 
-  }
-  return true;
-}
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
     if (eqArrays(array1, array2)) {
@@ -20,6 +8,4 @@ const assertArraysEqual = function(array1, array2) {
     }
   };
   
-assertArraysEqual([1, 2, 3], [1, 2, 3,]);
-assertArraysEqual([1, 2, 3], [1, '2', 3,]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+module.exports = assertArraysEqual;
